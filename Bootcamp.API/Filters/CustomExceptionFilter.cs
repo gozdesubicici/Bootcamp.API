340c5e9e180250a98b5927568f791cb45fee38c2
Gözdesu Bİçici <gzds.bcc@gmail.com>
2022-04-08 20:03:18 +0300

    CustomExceptionFilter eklendi

---
+++ Bootcamp.API/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Bootcamp.API.Filters
+{
+    public class CustomExceptionFilter : ExceptionFilterAttribute
+    {
+
+        public override Task OnExceptionAsync(ExceptionContext context)
+        {
+            context.ExceptionHandled = true; // Bu hatayı kendim ele alacağım demek.
+
+            Debug.WriteLine("Exception Filter çalıştı");
+             
+
+            return base.OnExceptionAsync(context);
+        }
+    }
+}
